cbbfb9dd1ff0e9babe92004e68bc19647f1404fa
swarty_d <swarty16@gmail.com>
2018-05-09 20:07:02 +0300

    update structure for push and checking

--- dist/js/main.js
+++ dist/js/main.js
@@ -1,57 +1,63 @@
-document.addEventListener('DOMContentLoaded', function () {
+document.addEventListener('DOMContentLoaded', function() {
 
-    // course logic
-    if (document.querySelector('.s_course__cards'))
 
-        // click on list element
-        $('.s_course__card').click(function (e) {
 
-            // create relation with block descr and view dataattribute
-            let node = e.currentTarget,
-                dataAtr = node.getAttribute('data-num');
+// course logic
+if(document.querySelector('.s_course__cards'))
 
-            // find block equal by data-num
-            let block = document.querySelector(`div[data-num="${dataAtr}"]`);
-            $('.s_course__skills').fadeOut();
-            $(block).fadeIn();
+   // click on list element
+   $('.s_course__card').click( function(e){
 
-            // add class for animate
-            $('.s_course__card').removeClass('s_course__card--active');
-            $(this).addClass('s_course__card--active');
-        });
+      // create relation with block descr and view dataattribute
+      let node = e.currentTarget,
+          dataAtr = node.getAttribute('data-num');
 
-    // activate rellax
-    let rellax = new Rellax('.rellax', {
-        center: true
-    });
+      // find block equal by data-num
+      let block = document.querySelector(`div[data-num="${dataAtr}"]`);
+      $('.s_course__skills').fadeOut();
+      $(block).fadeIn();
 
-    // soft svg logic
-    function softLogic() {
-        $('.soft__part1').click(function () {
-            $('div[data-block]').slideUp();
-            $('div[data-block="3"]').slideDown();
-        });
+      // add class for animate
+      $('.s_course__card').removeClass('s_course__card--active');
+      $(this).addClass('s_course__card--active');
+   })
 
-        $('.soft__part2').click(function () {
-            $('div[data-block]').slideUp();
-            $('div[data-block="1"]').slideDown();
-        });
 
-        $('.soft__part3').click(function () {
-            $('div[data-block]').slideUp();
-            $('div[data-block="2"]').slideDown();
-        });
+   // activate rellax
+   let rellax = new Rellax('.rellax', {
+      center: true
+   });
+
+
+// soft svg logic
+function softLogic(){
+    $('.soft__part1').click( function(){
+        $('div[data-block]').slideUp();
+        $('div[data-block="3"]').slideDown();
+    })
+
+    $('.soft__part2').click( function(){
+        $('div[data-block]').slideUp();
+        $('div[data-block="1"]').slideDown();
+    })
+
+    $('.soft__part3').click( function(){
+        $('div[data-block]').slideUp();
+        $('div[data-block="2"]').slideDown();
+    })
+}
+
+
+// enquire for svg in section soft
+
+
+enquire.register("screen and (min-width: 768px)", {
+    match : function() {
+        softLogic();
+    },
+    unmatch : function() {
+        // if unmatch
     }
-
-    // enquire for svg in section soft
-
-
-    enquire.register("screen and (min-width: 768px)", {
-        match: function () {
-            softLogic();
-        },
-        unmatch: function () {
-            // if unmatch
-        }
-    });
 });
+
+})
